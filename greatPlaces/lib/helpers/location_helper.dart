d1c19f56e0c9ca0f72c67a4cece4e599feaff296
Vincent Gagnon <vincent.gagnon93@gmail.com>
2020-09-17 18:24:17 +0200

    fix problem with google key

--- greatPlaces/lib/helpers/location_helper.dart
+++ greatPlaces/lib/helpers/location_helper.dart
@@ -1,7 +1,9 @@
+import '../internal/keys.dart' as keys;
+
 class LocationHelper {
   static String generateLocationPreviewImage(
     double latitude, double longitude
   ) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=AIzaSyBJ1xZg2EdiL36TdrRupIGB-dYtr8AO8uU';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=${keys.googleMapsKey}';
   }
 }
